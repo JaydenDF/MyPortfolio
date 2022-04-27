eecc130694fdbeed3ee97c01c63782b02bfddd6d
jayden fokkink <jaydenfokkink@gmail.com>
2022-04-27 12:46:01 +0200

    start off project, already begun and changed some things

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="bg-gray-200 dark:bg-slate-800 py-8 relative z-10">
+    <div class="container">
+        <div class="flex flex-wrap justify-between">
+            <span class="inline-flex items-center text-xl">
+                follow me on social media
+            </span>
+            <x-layout.social-icons class="mb-0"></x-layout.social-icons>
+        </div>
+    </div>
+</footer>
