997322b5431a1d2ed6aa01971a5a233880e1fc38
jayden fokkink <jaydenfokkink@gmail.com>
2022-04-30 21:05:59 +0200

    contact page without email sending.

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function submit(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'massage' => 'required'
+        ]);
+        // send email
+        return ['success' => true];
+    }
+}
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,8 @@
 
     <title>Laravel</title>
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Fonts -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
