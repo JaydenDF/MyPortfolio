0abfbae54f734e9fb5cce25db3d5e778e51525ac
jayden fokkink <jaydenfokkink@gmail.com>
2022-04-30 16:41:13 +0200

    done with portfolio section, only need to fill it with projects but that comes later.

--- app/View/Components/Home/Portfolio.php
+++ app/View/Components/Home/Portfolio.php
@@ -2,10 +2,13 @@
 
 namespace App\View\Components\Home;
 
+use Illuminate\Support\Arr;
 use Illuminate\View\Component;
 
 class Portfolio extends Component
 {
+    public array $items = [];
+    public array $tabs = [];
     /**
      * Create a new component instance.
      *S
@@ -13,7 +16,20 @@ class Portfolio extends Component
      */
     public function __construct()
     {
-        //
+        //TODO: fill in with items im proud off :)
+        $this->items = [
+            [
+                'category' => ['portfolio', 'laravel'],
+                'title' => 'this portfolio',
+                'image' => url('/images/galaxy_logo.png'),
+                'github' => 'https://github.com/JaydenDF/MyPortfolio'
+            ],
+            [
+                //2 and so on
+            ]
+        ];
+
+        $this->tabs = Arr::flatten(Arr::pluck($this->items, 'category'));
     }
 
     /**
